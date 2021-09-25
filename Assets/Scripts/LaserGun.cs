2f68ebe3740758a62426c58df532fb85c07b03fe
ReWriteGame <mr.ekstrimal1432@gmail.com>
2021-09-25 22:59:31 +0300

    add audio

--- Assets/Scripts/LaserGun.cs
+++ Assets/Scripts/LaserGun.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider2D))]
 public class LaserGun : MonoBehaviour
 {
     public Transform directionThrow;
+
+    public UnityEvent laserShotEnent;
 
     [SerializeField] private Bullet currentBullet;
     private void OnTriggerStay2D(Collider2D collision)
@@ -21,5 +24,6 @@ public class LaserGun : MonoBehaviour
     public void ThrowKnife()
     {
         currentBullet.gameObject.GetComponent<Rigidbody2D>().AddForce((directionThrow.position - transform.position).normalized * currentBullet.Force, ForceMode2D.Impulse);
+        laserShotEnent?.Invoke();
     }
 }
